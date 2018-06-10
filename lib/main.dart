25f5516b79ecfecc6bbdb09e37f375b305665f92
Aeon Tanvir <aeontanvir@gmail.com>
2018-06-11 00:27:02 +0600

    Add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'login_page.dart';
+import 'home_page.dart';
 
 void main() => runApp(new MyApp());
 
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lightBlue,
         fontFamily: 'Nunito',
       ),
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
